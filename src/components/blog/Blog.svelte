eecfbbb371a8572e1902dad0ba4240286b56a7ef
Taha ibn Munawar <taham@alpine.my.domain>
2024-10-04 10:35:38 +0500

    Blog Component Reimagined :sparkles:

--- src/components/blog/Blog.svelte
+++ src/components/blog/Blog.svelte
@@ -1,134 +1,158 @@
 <div class="blog">
-    <img class="banner" src="/blog/SnowycapsMilkyway.jpg" alt="Snowcaps Mountains with milkyway glaxy visible on the sky"/>
-    <h2 class="title">Alpine Linux Experience</h2>
-    <p class="description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint nobis atque beatae
-    </p>
-    <div class="border"></div>
-    <div class="tags">
-        <span>Linux</span>
-        <span>Alpine</span>
-        <span>Open-Rc</span>
-        <span>Musl-C</span>
+    <div class="banner">
+        <img src="/blog/SnowycapsMilkyway.jpg" alt="Snowcaps Mountains with milkyway glaxy visible on the sky"/>
+        <div class="fade-content">
+            <h2 class="title">Alpine Linux Experience</h2>
+            <p class="description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint nobis atque beatae
+            </p>
+            <footer>
+                <span class="tags">
+                    <span>Linux</span>
+                    <span>Alpine</span>
+                    <span>Open-Rc</span>
+                    <span>Musl-C</span>
+                </span>
+                <time>04 Oct 2024</time>
+                <span class="seperator">|</span>
+                <span class="dificulty hard">Hard</span>
+            </footer>
+        </div>
     </div>
-    <footer>
-        <time>04 Oct 2024</time>
-        <span>|</span>
-        <span class="dificulty hard">Hard</span>
-    </footer>
+    <!-- <div class="border"></div> -->
 </div>
 
 <style lang="scss">
 
 .blog {
-    display: grid;
-    grid-template:
-        "banner banner      banner banner banner" 1fr
-        ".      title       border tags   .     " fit-content(75px)
-        ".      description border tags   .     " fit-content(75px)
-        ".      description border footer .     " min-content
-        / 10px fit-content(220px) 1px min-content 5px;
-    row-gap: 10px;
-    column-gap: 5px;
-
     min-width: 350px;
-    height: 300px;
-    padding-bottom: 10px;
-
-    background-color: var(--verylow-contrast-accent-color);
-    border-radius: 10px;
+    width: 350px;
+    height: 250px;
     overflow: hidden;
+
+    border-radius: 10px;
     box-shadow: 3px 5px 5px 2px #0002;
 
+
     .banner {
-        grid-area: banner;
-
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-
-    }
-
-    .title {
-        grid-area: title;
-
-        font-family: var(--heading-font);
-        font-size: 1.2rem;
-        color: var(--high-contrast);
-        width: fit-content;
-
-        margin: 0;
-    }
-
-    .description {
-        grid-area: description;
-
-        font-size: 0.9rem;
-        color: var(--medium-contrast);
-        width: fit-content;
-
-        margin: 0;
-    }
-
-    .border {
-        grid-area: border;
-
-        height: 100%;
-        width: 1px;
-        background-image: radial-gradient(#0008 80%, #0000);
-    }
-
-    .tags {
-        grid-area: tags;
+        position: relative;
 
         width: 100%;
         height: 100%;
 
-        span {
-            display: inline-block;
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
 
-            font-size: 0.8rem;
-            color: white;
+        .fade-content {
+            position: absolute;
+            bottom: 0;
+            left: 0;
 
-            margin-bottom: 5px;
-            padding: 2px 3px;
+            width: 100%;
             height: fit-content;
+            padding: 10px 10px;
 
-            background-color: #222;
-            border-radius: 3px;
+            background-image: linear-gradient(to bottom, #0000, #0008 40px, #000a);
+            transition: backdrop-filter 0.5s;
+
+
+            .title {
+                font-family: var(--heading-font);
+                font-size: 1.2rem;
+                color: var(--hero-high-contrast);
+                width: fit-content;
+
+                margin: 20px 0 5px 0;
+            }
+
+            .description {
+                font-size: 0.9rem;
+                color: var(--hero-medium-contrast);
+                width: fit-content;
+
+                margin: 0 0 10px 0;
+            }
+
+            footer {
+                display: flex;
+                align-items: center;
+
+                min-width: max-content;
+                height: fit-content;
+                margin: 0 0 5px 0;
+
+                font-size: 0.8rem;
+
+                .tags {
+                    justify-self: flex-start;
+
+                    width: 100%;
+                    height: min-content;
+
+                    overflow-x: scroll;
+
+                    span {
+                        display: inline-block;
+
+                        color: var(--hero-high-contrast);
+
+                        padding: 2px 3px;
+                        height: fit-content;
+
+                        background-color: #caf3;
+                        border-radius: 3px;
+                    }
+                }
+
+                time {
+                    justify-self: flex-end;
+                    color: var(--hero-low-contrast);
+                    min-width: max-content;
+                }
+
+                .seperator {
+                    display: inline-block;
+                    padding-inline: 5px;
+                    color: var(--hero-medium-contrast);
+                }
+
+                .dificulty {
+                    display: inline-block;
+                    justify-self: flex-end;
+
+                    font-weight: 700;
+                    color: white;
+
+                    min-width: max-content;
+                    padding-inline: 3px;
+                    border-radius: 3px;
+
+                    &.easy {
+                        background-color: green;
+                    }
+
+                    &.normal {
+                        background-color: yellow;
+                    }
+
+                    &.hard {
+                        background-color: red;
+                    }
+                }
+            }
         }
+
+        &:hover {
+            .fade-content {
+                backdrop-filter: blur(10px);
+            }
+        }
+
     }
 
-    footer {
-        grid-area: footer;
-
-        min-width: max-content;
-        font-size: 0.8rem;
-
-        time {
-            color: var(--medium-contrast);
-        }
-
-        .dificulty {
-            display: inline-block;
-            font-weight: 700;
-            color: white;
-            padding-inline: 3px;
-            border-radius: 3px;
-
-            &.easy {
-                background-color: green;
-            }
-
-            &.normal {
-                background-color: yellow;
-            }
-
-            &.hard {
-                background-color: red;
-            }
-        }
-    }
 }
 
 </style>
